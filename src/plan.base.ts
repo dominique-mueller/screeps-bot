c2e1c70802b8d046963419d61a3462403f9c2dbc
Dominique Müller <dominique.m.mueller@gmail.com>
2022-05-11 22:04:50 +0200

    Optimize base link and storage placements

--- src/plan.base.ts
+++ src/plan.base.ts
@@ -1,5 +1,12 @@
 import { SB_Room, SB_Road, SB_Spawn, SB_RoomPosition } from './plan.interfaces';
-import { filterPositions, findAdjacentRoomPositionsForRoomPosition, findAdjacentRoomPositionsForRoomPositions } from './plan.utilities';
+import {
+  filterPositions,
+  findAdjacentRoomPositionsForRoomPosition,
+  findAdjacentRoomPositionsForRoomPositions,
+  findDirectPathForPlanning,
+  sortPathPositionsByLength,
+} from './plan.utilities';
+import { flattenArray } from './utilities';
 
 /**
  * Plan base
@@ -8,7 +15,7 @@ import { filterPositions, findAdjacentRoomPositionsForRoomPosition, findAdjacent
  * - Place link close to other links
  *
  * TODO: Long term
- * - Handle creating new spawn (e.g. capturing additional room)
+ * - Handle creating new base without existing spawn (e.g. capturing additional room)
  *
  * @param   room    Room
  * @param   roomMap Room map (will be mutated in place)
@@ -17,29 +24,45 @@ export const planBase = (room: Room, roomMap: SB_Room): void => {
   // Find spawn
   const initialSpawn: StructureSpawn = room.find(FIND_MY_SPAWNS)[0];
 
-  // Find base center position
-  const baseCenterPosition: SB_RoomPosition = { position: room.getPositionAt(initialSpawn.pos.x, initialSpawn.pos.y - 1) as RoomPosition };
+  // Find base position and its adjacent positions
+  const basePosition: SB_RoomPosition = { position: room.getPositionAt(initialSpawn.pos.x, initialSpawn.pos.y - 1) as RoomPosition };
+  const baseAdjacentPositions: Array<SB_RoomPosition> = findAdjacentRoomPositionsForRoomPosition(room, basePosition);
 
-  // Assign structures to positions
-  //
-  // Notes:
-  // - Spawns are as spread out as possible
-  // - On tower per side
-  // - Other structures in between
-  const baseCenterAdjacentPositions: Array<SB_RoomPosition> = findAdjacentRoomPositionsForRoomPosition(room, baseCenterPosition);
-  const spawnPositions: Array<SB_RoomPosition> = [baseCenterAdjacentPositions[1], baseCenterAdjacentPositions[7]];
-  const linkPositions: Array<SB_RoomPosition> = [baseCenterAdjacentPositions[3]];
-  const towerPositions: Array<SB_RoomPosition> = [baseCenterAdjacentPositions[2], baseCenterAdjacentPositions[6]];
-  const storagePositions: Array<SB_RoomPosition> = [baseCenterAdjacentPositions[5]];
-  const reserved: Array<SB_RoomPosition> = [baseCenterAdjacentPositions[0]];
-  const roadPositions: Array<SB_RoomPosition> = filterPositions(
-    findAdjacentRoomPositionsForRoomPositions(room, baseCenterAdjacentPositions),
-    [baseCenterPosition],
-  );
+  // Find spawn and tower positions (equally spread based on existing spawn)
+  const spawnPositions: Array<SB_RoomPosition> = [baseAdjacentPositions[1], baseAdjacentPositions[7]];
+  const towerPositions: Array<SB_RoomPosition> = [baseAdjacentPositions[2], baseAdjacentPositions[6]];
+
+  // Find link, storage and reserved position (based on shortest distance to all sources)
+  const sources: Array<Source> = room.find(FIND_SOURCES);
+  const unusedBaseAdjacentPositions: Array<SB_RoomPosition> = [
+    baseAdjacentPositions[0],
+    baseAdjacentPositions[3],
+    baseAdjacentPositions[5],
+  ];
+  const sortedAvailableBaseAdjacentPositions: Array<Array<SB_RoomPosition>> = unusedBaseAdjacentPositions
+    .map((baseAdjacentPosition: SB_RoomPosition): Array<SB_RoomPosition> => {
+      // Combine all paths between sources and base adjacent position
+      return flattenArray(
+        sources.map((source: Source): Array<SB_RoomPosition> => {
+          // Find path between source and base adjacent position
+          return findDirectPathForPlanning(room, { position: source.pos }, baseAdjacentPosition);
+        }),
+      );
+    })
+    // Find shortest path by sorting by sum of paths between source and base adjacent position
+    .sort(sortPathPositionsByLength);
+  const linkPosition: SB_RoomPosition = sortedAvailableBaseAdjacentPositions[0].slice(-1)[0];
+  const storagePosition: SB_RoomPosition = sortedAvailableBaseAdjacentPositions[1].slice(-1)[0];
+  const reservedPosition: SB_RoomPosition = sortedAvailableBaseAdjacentPositions[2].slice(-1)[0];
+
+  // Find road positions
+  const roadPositions: Array<SB_RoomPosition> = filterPositions(findAdjacentRoomPositionsForRoomPositions(room, baseAdjacentPositions), [
+    basePosition,
+  ]);
 
   // Update room map
-  roomMap.base = baseCenterPosition;
-  roomMap.reserved.push(...reserved);
+  roomMap.base = basePosition;
+  roomMap.reserved.push(reservedPosition);
   roomMap.spawns.push(
     {
       id: initialSpawn.id,
@@ -52,9 +75,9 @@ export const planBase = (room: Room, roomMap: SB_Room): void => {
       };
     }),
   );
-  roomMap.links.push(...linkPositions);
+  roomMap.links.push(linkPosition);
   roomMap.towers.push(...towerPositions);
-  roomMap.storages.push(...storagePositions);
+  roomMap.storages.push(storagePosition);
   roomMap.roads.push(
     ...roadPositions.map((adjacentPosition: SB_RoomPosition): SB_Road => {
       return {
--- src/plan.ts
+++ src/plan.ts
@@ -1,13 +1,3 @@
-/**
- * TODO: Out of scope for now
- *
- * - Create spawn (if not initial room)
- * - Handle room variants,
- *   - main room (controller)
- *   - mining room (sources)
- *   - travel room (nothing)
- */
-
 import { planBase } from './plan.base';
 import { planSources } from './plan.sources';
 import { planController } from './plan.controller';
@@ -21,6 +11,12 @@ import { SB_Room } from './plan.interfaces';
 
 /**
  * Plan room
+ *
+ * TODO: Not handles: Room variants, in particular
+ * - main room (controller)
+ *   - initial room (spawn does exist) vs new room (spawn does not exist)
+ * - mining room (sources)
+ * - travel room (nothing)
  */
 export const planRoom = (room: Room): void => {
   // INIT
@@ -39,8 +35,6 @@ export const planRoom = (room: Room): void => {
   } as any as SB_Room;
 
   // RUN
-  // TODO: How to save building priority (only temp?)
-  // TODO: How to save connections? (e.g. link of source vs. link of XYZ)
   planExits(room, roomMap);
   planBase(room, roomMap);
   planController(room, roomMap);
